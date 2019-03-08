a1130172ef17be7b96b9301b5f37855f0af24c5c
Daniel Watson <daniel.watson@nyu.edu>
2019-03-08 19:06:16 +0400

    Added setup for package

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setup script for the tfbuild package."""
+
+from setuptools import setup, find_packages
+
+
+VERSION = "0.1.0"
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setup(
+    name="tf-inputs",
+    version=VERSION,
+    author="Daniel Watson",
+    author_email="daniel.watson@nyu.edu",
+    url="https://github.com/danielwatson6/tf-inputs.git",
+    description="Input pipelines for TensorFlow that make sense.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=open("requirements.txt").read().split(),
+)
